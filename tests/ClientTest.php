0e67923fc9becd77bc7563161c93ad59d2f3c6af
piksera <git@piksera.com>
2024-09-07 11:32:17 +0300

    1.0

---
+++ tests/ClientTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace PikseraPackages\ComposerClient;
+
+use PHPUnit\Framework\TestCase;
+use PikseraPackages\ComposerClient\Client;
+
+class ClientTest extends TestCase
+{
+    public function testMarketplaceIndex()
+    {
+        $composerClient = new Client();
+        $composerSearch = $composerClient->search();
+
+        foreach($composerSearch as $packageName=>$versions) {
+            if (!is_array($versions)) {
+                continue;
+            }
+            foreach($versions as $version) {
+                if (strpos($version['name'], 'template') !== false) {
+                    $this->assertNotEmpty($version['extra']['_meta']['screenshot']);
+                    $this->assertNotEmpty($version['dist']['url']);
+                }
+            }
+        }
+
+    }
+
+}
